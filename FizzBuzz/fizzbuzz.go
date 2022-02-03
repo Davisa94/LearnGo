435b746cc28a6faed6f9d5c0192f413c7ad86d80
Davisa94 <Austin.i.davis94@gmail.com>
2022-02-02 16:41:39 -0800

    Begin writing printer funciton

---
+++ FizzBuzz/fizzbuzz.go
@@ -0,0 +1,27 @@
+/**********************************************************
+# Author: Austin Benitez
+# Date: 2/2/22
+# Assignment: Fizz Buzz
+# Description:
+# The rules: Count to 100*
+# Every Multiple of 3 is replaced with Fizz
+# Every multiple of 5 is replaced with Buzz
+# Every multiple of both is replaced with FizzBuzz
+# Stretch: every other output is indented
+***********************************************************/
+package main
+
+import (
+	"fmt"
+)
+
+func arrayPrinter(fizzyWord string) {
+	for _, item := range fizzyWord {
+		fmt.Println(item)
+	}
+
+}
+
+func main() {
+	fmt.Println("e")
+}
